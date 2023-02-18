c0af6f43ef98bd40048b468f0b51d717739a3d7f
hummingbirdcreative <103891227+hummingbirdcreative@users.noreply.github.com>
2023-02-18 06:26:51 -1000

    Update settings.py

--- Vita/settings.py
+++ Vita/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'db',
-        'PORT': 0.0.0.0:$PORT,
+        'PORT': 0.0.0.0:$PORT
     }
 }
 
